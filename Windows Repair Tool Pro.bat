7d4b9894191312d18ad53386b8c234a368fdbeab
Hesham Taha <116387396+newmatrix@users.noreply.github.com>
2026-05-20 02:27:00 +0300

    Update Windows Repair Tool Pro.bat

--- Windows Repair Tool Pro.bat
+++ Windows Repair Tool Pro.bat
@@ -7,7 +7,7 @@
 set CURRENT_VERSION=1.2
 
 set VERSION_URL=https://raw.githubusercontent.com/newmatrix/WinRTP/main/Version.txt
-set TOOL_URL=https://raw.githubusercontent.com/newmatrix/WinRTP/main/Windows%%20Repair%%20Tool%%20Pro.bat
+set TOOL_URL=https://raw.githubusercontent.com/newmatrix/WinRTP/main/Windows%20Repair%20Tool%20Pro.bat
 
 set TEMP_VERSION=%temp%\Version.txt
 set NEW_TOOL=%temp%\Windows_Repair_Tool_Pro.bat
