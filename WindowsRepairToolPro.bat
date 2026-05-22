a1c285fd5253edcabe770519b7880af761e990a4
Hesham Taha <116387396+newmatrix@users.noreply.github.com>
2026-05-22 15:48:43 +0300

    Update WindowsRepairToolPro.bat

--- WindowsRepairToolPro.bat
+++ WindowsRepairToolPro.bat
@@ -712,31 +712,6 @@ if exist "%AUTO_TEMP_VER%" (
     )
     del "%AUTO_TEMP_VER%" >nul 2>&1
 )
-goto menu
-
-:silent_check_update
-set "AUTO_CURR_VER=1.1"
-set "AUTO_VER_URL=https://raw.githubusercontent.com/newmatrix/WinRTP/main/Version.txt"
-set "AUTO_TEMP_VER=%temp%\SilentVersion.txt"
-
-if exist "%AUTO_TEMP_VER%" del "%AUTO_TEMP_VER%" >nul 2>&1
-
-powershell -NoProfile -ExecutionPolicy Bypass -Command "(New-Object Net.WebClient).DownloadFile('%AUTO_VER_URL%', '%AUTO_TEMP_VER%')" >nul 2>&1
-
-timeout /t 1 >nul 2>&1
-
-if exist "%AUTO_TEMP_VER%" (
-    set "ONLINE_VER_FOUND="
-    for /f "delims=" %%i in ('type "%AUTO_TEMP_VER%"') do (set "ONLINE_VER_FOUND=%%i")
-    
-    if defined ONLINE_VER_FOUND set "ONLINE_VER_FOUND=!ONLINE_VER_FOUND: =!"
-    
-    if defined ONLINE_VER_FOUND if "!ONLINE_VER_FOUND!" neq "%AUTO_CURR_VER%" (
-        set "UPDATE_ALERT=YES"
-        set "NEW_VERSION_NUM=!ONLINE_VER_FOUND!"
-    )
-    del "%AUTO_TEMP_VER%" >nul 2>&1
-)
 goto :eof
 
 :UPDATE
